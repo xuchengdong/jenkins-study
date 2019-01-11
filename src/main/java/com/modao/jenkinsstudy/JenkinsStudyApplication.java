a7d3678cf79f1660887b764d19e49a8c2e03dc7f
xuchengdong <xuchengdongxcd@126.com>
2019-01-11 14:01:15 +0800

    initial project

---
+++ src/main/java/com/modao/jenkinsstudy/JenkinsStudyApplication.java
@@ -0,0 +1,14 @@
+package com.modao.jenkinsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsStudyApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/modao/jenkinsstudy/JenkinsStudyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.modao.jenkinsstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsStudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
